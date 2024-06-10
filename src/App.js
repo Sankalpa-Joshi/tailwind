194ede58475503bd4d46d72225ad56dc6ce9bbe5
Sankalpa-Joshi <sankalpajoshi351@gmail.com>
2024-06-10 17:48:44 +0545

    new branch test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <div className="bg-black text-white">
         <div className="flex ...">
-          <div className="flex-1 h-16 ...">01</div>
+          <div className="flex-1 h-16 outline-white">01</div>
           <div className="shrink h-16 w-32 ...">02</div>
           <div className="flex-1 h-16 ...">03</div>
         </div>
